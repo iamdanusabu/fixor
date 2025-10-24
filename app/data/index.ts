cf0f7d2af6cf589df30f49ac9c036cc57472f821
dansabu15 <dansabu15@gmail.com>
2025-10-24 18:46:24 +0000

    ghjkl

--- app/data/index.ts
+++ app/data/index.ts
@@ -1,8 +1,5 @@
-// app/data/blog/index.ts
 
 import firstPost from './blog/first-post';
 import secondPost from './blog/second-post';
-// --- Import any new posts you create here ---
 
-// Add all imported posts to this array
 export const allPosts = [firstPost, secondPost];
